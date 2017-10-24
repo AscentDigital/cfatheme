bc918e4c61bd8982f7a7d5df708ad1e62c0c562b
Stephan Sutter <stephan.sutter2000@gmail.com>
2017-10-24 14:53:23 +0800

    Added faqs page

---
+++ page-faqs.php
@@ -0,0 +1,109 @@
+<?php get_header(); ?>
+<!-- .secondary-banner -->
+<div class="secondary-banner" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/bannerinterior5-2.jpg);">
+	<div class="container">
+		<h1>FAQ's</h1>
+	</div>
+</div>
+<!-- end .secondary-banner  -->
+
+
+<!-- #form -->
+<section id="about" class="section border-block __up">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-11">
+				<div class="faq-area">
+					<h1>Frequently Asked Questions</h1>
+					<div class="faq-list">
+						<div class="media">
+							<div class="media-left">
+								<span class ="faq-number">01</span>
+							</div>
+							<div class="media-body">
+								<h4 class="faq-header">How do I know I need the services of CFA?</h4>
+								<h4 class ="faq-content">Are you experiencing thirty to sixty day lag times in having your construction draw request/account receivable reviewed, approved and funded by the prime or public sector unit?  Accordingly, CFA is prepared to advance funds to you under an approved line of credit to your company, within five days of your draw request/account receivable being submitted to the prime or public sector.</h4>
+							</div>
+						</div>
+						<!-- -->
+
+						<div class="media">
+							<div class="media-left">
+								<span class ="faq-number">02</span>
+							</div>
+							<div class="media-body">
+								<h4 class="faq-header">How do I get funds into my account?</h4>
+								<h4 class ="faq-content">After your request is reviewed and approved by CFA, the funds requested will be wired into your operating account by 2:00PM on the day of final approval.</h4>
+							</div>
+						</div>
+						<!-- -->
+
+						<div class="media">
+							<div class="media-left">
+								<span class ="faq-number">03</span>
+							</div>
+							<div class="media-body">
+								<h4 class="faq-header">How do I know if I am a candidate for a loan?</h4>
+								<h4 class ="faq-content">
+								If you are a small contractor or a small prime which operates on a very tight cash flow and which has demonstrated a strong, honest and competent reputation, you qualify as a candidate for the services of CFA.</h4>
+							</div>
+						</div>
+						<!-- -->
+
+						<div class="media">
+							<div class="media-left">
+								<span class ="faq-number">04</span>
+							</div>
+							<div class="media-body">
+								<h4 class="faq-header">Does the funding process create an additional operational commitment in my company?</h4>
+								<h4 class ="faq-content">
+								The request process includes the preparation of a one page advance calculation coupled with a copy of the draw request/account receivable being submitted to the prime or public sector unit.</h4>
+							</div>
+						</div>
+						<!-- -->
+
+						<div class="media">
+							<div class="media-left">
+								<span class ="faq-number">05</span>
+							</div>
+							<div class="media-body">
+								<h4 class="faq-header">
+									How much can I barrow?
+								</h4>
+								<h4 class ="faq-content">
+								The line of credit is limited to a total of $500K subject to no more than four draws against the line of credit.</h4>
+							</div>
+						</div>
+						<!-- -->
+
+						<div class="media">
+							<div class="media-left">
+								<span class ="faq-number">06</span>
+							</div>
+							<div class="media-body">
+								<h4 class="faq-header">What is the cost of the line of credit?</h4>
+								<h4 class ="faq-content">CFA charges a 5% facility fee.  The facility fee is netted out of the advance request resulting in only 95% of the advance request being funded.    If the advance is not paid back by the end of sixty days, then on the sixty-first day interest will accrue at Prime + 10% for the next thirty days; at which time the advance must be paid off.  If the advance is paid off prior to sixty days, CFA will refund 1.25% of the facility fee.</h4>
+							</div>
+						</div>
+						<!-- -->
+
+						<div class="media">
+							<div class="media-left">
+								<span class ="faq-number">07</span>
+							</div>
+							<div class="media-body">
+								<h4 class="faq-header">Are there any additional costs?</h4>
+								<h4 class ="faq-content">
+									In addition to a facility fee and an interest rate commencing on the sixty-first day of the advance, there is an application fee of $250 and a document preparation fee of $500.
+								</h4>
+							</div>
+						</div>
+						<!-- -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+    <!-- end #form -->
+<?php get_footer(); ?>
